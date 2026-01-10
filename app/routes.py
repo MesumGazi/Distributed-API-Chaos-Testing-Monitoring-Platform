75e745873e5ce2f857dafa8e5edcc29c5d426a1e
unknown <gazi.xyzx@gmail.com>
2026-01-10 23:57:52 +0530

    gitlab variable added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 import httpx
 import asyncio
 from fastapi import FastAPI
-from .services.services import url_validation
-from .config.config import setting
+from services.services import url_validation
+from config.config import setting
 app=FastAPI()
 
 
@@ -12,14 +12,27 @@ urls=[setting.url]*setting.attempts
 async def is_google_up():
 
     semaphore = asyncio.Semaphore(setting.max_concurrency_limit)
-    async with httpx.AsyncClient(timeout=0.5) as client:
+    async with httpx.AsyncClient(timeout=5.0) as client:
         async def semaphore_limit(url):
             async with semaphore:
                 return await url_validation(url,client)
         tasks=[semaphore_limit(url) for url in urls]
         results = await asyncio.gather(*tasks)
+        healthy_count= sum( 1 for r in results if r.get("status_code",0)<500)
+        response_time = [r["elapsed_time"] for r in results if "elapsed_time" in r]
 
-    return results
+    return {
+        "summary":{
+            "total_checks": len(results),
+            "healthy_url_count": healthy_count,
+            "unhealthy_url_count" : len(results) - healthy_count,
+            "avg_response_time": sum(response_time)/len(response_time) if response_time else 0,
+            "max_response_time": max(response_time) if response_time else 0
+
+
+        },
+    "results": results
+    }
 
 
 
--- app/services/services.py
+++ app/services/services.py
@@ -1,7 +1,7 @@
 import httpx
 
 
-async def url_validation(url,client=httpx.AsyncClient):
+async def url_validation(url,client:httpx.AsyncClient):
     try:   
             response = await client.get(url)
 
@@ -16,10 +16,29 @@ async def url_validation(url,client=httpx.AsyncClient):
                     response.headers.get('location') if response.status_code == 302 else
                     "Success")
             }
+    except httpx.TimeoutException:
+           return{
+                  "url":url,
+                  "Status": False,
+                  "error":"request Timed out"
+           }
+    except httpx.HTTPStatusError as e:
+           return{
+                  "url":url,
+                  "status": False,
+                  "error":f"http error{e.response.status_code}"
+           }
+    except httpx.NetworkError as e:
+           return{
+                "url":url,
+                "status": False,
+                "error":f"Network Error {str(e)}"
+                  
+           }
     except Exception as e:
             return {
                 "url": url,
                 "status": False,
-                "error":str(e)
+                "error":f"unexpected error{str(e)}"
             }
 
